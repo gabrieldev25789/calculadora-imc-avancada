af0fd4855b039365bd8eacbb162f6ded4fd585d5
Gabriel <gabrielsb1090@gmail.com>
2025-10-01 16:15:45 -0300

    Renomeando pasta

---
+++ js/calcTreinos.js
@@ -0,0 +1,170 @@
+
+
+  const treinosPerder = {
+    perder: {
+      segunda:{
+      peito: [
+          "Supino Reto com halteres: 3x12",
+          "Crucifixo reto: 3x15 ( Peito )",
+        ],
+        ombro: [
+          "Elevação lateral: 3x15",
+          "Desenvolvimento com halteres: 3x12 ( Ombro )"
+        ],
+        triceps: [
+          "Tríceps polia: 3x15",
+          "Tríceps coice: 3x15 ( Tríceps )"
+        ]
+      },
+      terca: {
+        costas: [
+          "Remada baixa: 3x12 ",
+          "Puxada frontal: 3x15 ( Costas )"
+        ],
+        biceps: [
+          "Rosca direta: 3x15 ",
+          "Rosca martelo: 3x15 ( Biceps )"
+        ]
+      },
+      quarta: {
+        pernaCompleto: [
+          "Agachamento livre: 3x12",
+          "Leg press: 3x15",
+          "Cadeira extensora: 3x15",
+          "Flexora: 3x15",
+          "Panturrilha: 3x20"
+        ]
+      },
+      quinta: {
+        ombroIsolado: [
+          "Elevação lateral: 3x15",
+          "Elevação frontal: 3x15",
+          "Crucifixo inverso: 3x15"
+        ]
+      },
+      sexta: {
+        biceps: [
+          "Rosca direta com barra: 3x15",
+          "Rosca concentrada: 3x12 ( Biceps )"
+        ],
+        triceps: [
+          "Tríceps testa: 3x15",
+          "Mergulho em banco: 3x15 ( Tríceps )"
+        ]
+      }
+    },
+  
+    manter: {
+      segunda: {
+        peito: [
+          "Supino Reto: 3x10",
+          "Supino Inclinado: 3x10",
+          "Crucifixo Inclinado: 3x12"
+        ],
+        ombro: [
+          "Desenvolvimento: 3x10",
+          "Elevação Lateral: 3x12"
+        ],
+        triceps: [
+          "Tríceps polia: 3x12",
+          "Tríceps francês: 3x10"
+        ]
+      },
+      terca: {
+        costas: [
+          "Puxada na frente: 3x10",
+          "Remada curvada: 3x10"
+        ],
+        biceps: [
+          "Rosca direta: 3x10",
+          "Rosca alternada: 3x12"
+        ]
+      },
+      quarta: {
+        pernaCompleto: [
+          "Agachamento: 3x10",
+          "Avanço com halteres: 3x12 cada perna",
+          "Leg press: 3x12",
+          "Extensora e Flexora: 3x12",
+          "Panturrilha: 3x20"
+        ]
+      },
+      quinta: {
+        ombroIsolado: [
+          "Elevação lateral: 3x12",
+          "Elevação frontal: 3x12",
+          "Desenvolvimento unilateral: 3x10"
+        ]
+      },
+      sexta: {
+        biceps: [
+          "Rosca direta: 3x10",
+          "Rosca martelo: 3x12"
+        ],
+        triceps: [
+          "Tríceps testa: 3x10",
+          "Tríceps banco: 3x12"
+        ]
+      }
+    },
+  
+    ganhar: {
+      segunda: {
+        peito: [
+          "Supino Reto com barra: 4x8",
+          "Supino Inclinado: 4x8",
+          "Crossover: 4x10"
+        ],
+        ombro: [
+          "Desenvolvimento com barra: 4x10",
+          "Elevação lateral: 4x12"
+        ],
+        triceps: [
+          "Tríceps polia: 4x12",
+          "Tríceps corda: 4x12"
+        ]
+      },
+      terca: {
+        costas: [
+          "Puxada na frente: 4x10",
+          "Remada curvada: 4x10",
+          "Pulldown: 4x12"
+        ],
+        biceps: [
+          "Rosca direta com barra: 4x10",
+          "Rosca concentrada: 4x12"
+        ]
+      },
+      quarta: {
+        pernaCompleto: [
+          "Agachamento livre: 4x10",
+          "Leg press pesado: 4x12",
+          "Cadeira extensora: 4x12",
+          "Flexora: 4x12",
+          "Stiff com halteres: 4x10",
+          "Panturrilha: 4x20"
+        ]
+      },
+      quinta: {
+        ombroIsolado: [
+          "Desenvolvimento Arnold: 4x10",
+          "Elevação lateral: 4x12",
+          "Crucifixo inverso: 4x12",
+          "Remada alta: 4x10"
+        ]
+      },
+      sexta: {
+        biceps: [
+          "Rosca direta: 4x10",
+          "Rosca 21: 3x",
+          "Rosca martelo: 4x10"
+        ],
+        triceps: [
+          "Tríceps testa: 4x10",
+          "Tríceps banco: 4x12",
+          "Tríceps polia com corda: 4x12"
+        ]
+      }
+    }
+  };  
+export default treinos;
